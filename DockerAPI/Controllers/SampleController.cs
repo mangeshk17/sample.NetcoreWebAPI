c19e766fa9073578770cd5faf9fa7fcb7bfb95ea
mangeshk17 <mangesh.kulkarni170887@gmail.com>
2022-01-18 11:17:19 +0530

    Project Solution added
    Project Solution added

---
+++ DockerAPI/Controllers/SampleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        public IActionResult TestMethod()
+        {
+            string query = "http://localhost:9000/";
+            return Ok(query);
+        }
+    }
+}
